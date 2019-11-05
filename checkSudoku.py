e908f1b8d9f498d95b4a743ac1e7a8de326ea59c
BerkanBR <bbeyhanov@cifpfbmoll.eu>
2019-11-05 18:27:58 +0100

    Preparando casos test para el Sudoku.

---
+++ checkSudoku.py
@@ -0,0 +1,28 @@
+from checkCuadrado import checkCuadrado
+from checkNumerosValidos import checkNumerosValidos
+from checkFilas import checkFilas
+from checkColumnas import checkColumnas
+
+
+def check_sudoku(sudoku):
+
+    return checkCuadrado(sudoku) and checkNumerosValidos(sudoku) \
+        and checkFilas(sudoku) and checkColumnas(sudoku)
+
+
+if __name__ == '__main__':
+
+    import casosTestSudoku
+
+    for attr in sorted(casosTestSudoku.__dict__):
+        # Scan namespace keys (or enumerate) del objeto modulo checkCuadrado
+        # Asi podemos añadir todos los casos que queramos
+        # en la unidad cassTestSudoku sin modificar este codigo
+        if attr.startswith('__'):
+            pass
+            # Skip atributo
+        else:
+            print(attr, " => ", check_sudoku(casosTestSudoku.__dict__[attr]))
+            # mismo codigo que getattr(module, attr)
+            # es necesario añadir el espacio de nombres del modulo:
+            # casosTestSudoku.irregular
